b26397bf6d7176fb7653923b752c169adaed5e18
hza81007155 <800755@qq.com>
2026-08-09 15:18:12 +0800

    Remove MosDNS from luci applications
    Removed the MosDNS application from the feeds.

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,6 @@ rm -rf feeds/luci/applications/luci-app-lucky
 rm -rf feeds/luci/applications/luci-app-wol
 rm -rf feeds/luci/applications/luci-app-vlmcsd
 rm -rf feeds/luci/applications/luci-app-accesscontrol
-rm -rf feeds/luci/applications/luci-app-MosDNS
 rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/packages/net/geoview
 rm -rf feeds/packages/net/sing-box
